d52a02b9effe71352cf252c8c5b86f68bff2b787
Elena <elenarock28@gmail.com>
2024-05-20 18:58:11 +0300

    add BCrypt for passwords

--- src/main/java/telran/java52/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/telran/java52/accounting/service/UserAccountServiceImpl.java
@@ -3,6 +3,7 @@ package telran.java52.accounting.service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import telran.java52.accounting.dto.RolesDto;
 import telran.java52.accounting.dto.UserDto;
 import telran.java52.accounting.dto.UserEditDto;
 import telran.java52.accounting.dto.UserRegisterDto;
+import telran.java52.accounting.exeption.IncorrectRoleExeption;
 import telran.java52.accounting.exeption.UserExistsException;
 import telran.java52.accounting.exeption.UserNotFoundExeption;
 import telran.java52.accounting.model.UserAccount;
@@ -29,6 +31,9 @@ public class UserAccountServiceImpl implements UserAccountService {
 			throw new UserExistsException();
 		}
 		UserAccount userAccount = modelMapper.map(user, UserAccount.class);
+		String password = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()); 
+		// зашифровать пароль перед тем как отправить в базу
+		userAccount.setPassword(password);
 		userAccount = userRepository.save(userAccount);
 		return modelMapper.map(userAccount, UserDto.class);
 	}
@@ -63,13 +68,18 @@ public class UserAccountServiceImpl implements UserAccountService {
 	public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
 		UserAccount userAccount = userRepository.findById(login).orElseThrow(UserNotFoundExeption::new);
 		boolean res;
-		if (isAddRole) {
-			res = userAccount.addRole(role);
-		} else {
-			res = userAccount.removeRole(role);
-		}
-		if (res) {
-			userRepository.save(userAccount);
+		//прверка роли, если такой роли не предусмотрена в инам то нужно бросить ошибку 
+		try {
+			if (isAddRole) {
+				res = userAccount.addRole(role);
+			} else {
+				res = userAccount.removeRole(role);
+			}
+			if (res) {
+				userRepository.save(userAccount);
+			} 
+		} catch (Exception e) {
+			throw new IncorrectRoleExeption();
 		}
 		Set<String> roleSet = userAccount.getRoles().stream().map(r -> r.toString()).collect(Collectors.toSet());
 		return new RolesDto(login, roleSet);
@@ -78,8 +88,10 @@ public class UserAccountServiceImpl implements UserAccountService {
 	@Override
 	public void changePassword(String login, String newPassword) {
 		UserAccount userAccount = userRepository.findById(login).orElseThrow(UserNotFoundExeption::new);
-		userAccount.setPassword(newPassword);
-		userRepository.save(userAccount);
+		String password = BCrypt.hashpw(newPassword, BCrypt.gensalt()); 
+		// зашифровать пароль перед тем как отправить в базу
+		userAccount.setPassword(password);
+		userAccount = userRepository.save(userAccount);
 	}
 
 }
